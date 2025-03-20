63fde3f141e7dc497e4feadeae82538885fcb4b0
Avanito <98200489+Avanito@users.noreply.github.com>
2025-03-19 20:45:35 -0400

    Update models.py
    This is an addition to original code to check if current user has a profile when signing in. If not then at the signing in the computer will create a default profile

--- Django_Blog/09-Update-User-Profile/django_project/users/models.py
+++ Django_Blog/09-Update-User-Profile/django_project/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -10,7 +11,7 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
+    def save(self, *args, **kargs):
         super().save()
 
         img = Image.open(self.image.path)
@@ -19,3 +20,12 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
+
+    def no_profile(self,*args,**Kargs):
+        try:
+            picture = Image.open(self.profile.image)
+            from_signal = 'False'
+        except AttributeError:
+            from_signal = 'True'
+        return(from_signal)
+            
